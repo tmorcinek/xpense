08c44c5f023fb9562d4c0c9d4b30de9c42316aff
Tomasz Morcinek <tomasz@morcinek.co.uk>
2016-01-02 13:15:02 +0700

    Add Expense Model and Manager.

---
+++ app/src/main/kotlin/com/morcinek/xpense/common/expense/ExpenseManager.kt
@@ -0,0 +1,13 @@
+package com.morcinek.xpense.common.expense
+
+import com.morcinek.xpense.common.expense.model.Expense
+
+/**
+ * Copyright 2015 Tomasz Morcinek. All rights reserved.
+ */
+class ExpenseManager {
+
+    fun addExpense(expense: Expense) {
+        TODO()
+    }
+}
---
+++ app/src/main/kotlin/com/morcinek/xpense/common/expense/model/Expense.kt
@@ -0,0 +1,13 @@
+package com.morcinek.xpense.common.expense.model
+
+import java.util.*
+
+/**
+ * Copyright 2015 Tomasz Morcinek. All rights reserved.
+ */
+data class Expense(val value: Float, val currency: String, val title: String,
+                   val tags: Set<String>, val date: Date) : Comparable<Expense> {
+    override fun compareTo(other: Expense) = (value - other.value + 0.5).toInt()
+
+    operator fun plus(expense: Expense) = value + expense.value
+}
