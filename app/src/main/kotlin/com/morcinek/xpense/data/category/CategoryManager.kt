fffec7e547d9f2d61ba1a7921b64b69af794b832
Tomasz Morcinek <tomasz@morcinek.co.uk>
2016-01-22 18:03:45 +0700

    Adding Accept Button to text picker dialog fragment.

--- app/src/main/kotlin/com/morcinek/xpense/data/category/CategoryManager.kt
+++ app/src/main/kotlin/com/morcinek/xpense/data/category/CategoryManager.kt
@@ -1,11 +1,21 @@
 package com.morcinek.xpense.data.category
 
-import com.morcinek.xpense.data.category.Category
+import java.util.*
 
 /**
  * Copyright 2016 Tomasz Morcinek. All rights reserved.
  */
 class CategoryManager {
 
-    fun getCategories() = listOf(Category("Food and Drinks", 0xFFFF0000.toInt()), Category("Alcohol", 0xAAFFBB00.toInt()), Category("Apartment", 0xff00ff00.toInt()))
+    private val categories: MutableList<Category> = ArrayList()
+
+    init {
+        categories.addAll(listOf(Category("Food and Drinks", 0xFFFF0000.toInt()), Category("Alcohol", 0xAAFFBB00.toInt()), Category("Apartment", 0xff00ff00.toInt())))
+    }
+
+    fun getCategories(): List<Category> = categories
+
+    fun addCategory(category: Category) {
+        categories.add(category)
+    }
 }
