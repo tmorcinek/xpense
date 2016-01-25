749be2f14b7663faf61af7463cde23a8f8588213
Tomasz Morcinek <tomasz@morcinek.co.uk>
2016-01-25 22:55:59 +0700

    Adding GooglePlayServices.

--- app/src/main/kotlin/com/morcinek/xpense/project/ProjectActivity.kt
+++ app/src/main/kotlin/com/morcinek/xpense/project/ProjectActivity.kt
@@ -1,6 +1,11 @@
 package com.morcinek.xpense.project
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
+import com.google.android.gms.location.places.AutocompleteFilter
+import com.google.android.gms.location.places.ui.PlaceAutocomplete
 import com.morcinek.xpense.Application
 import com.morcinek.xpense.R
 import com.morcinek.xpense.common.utils.getParcelable
@@ -10,6 +15,7 @@ import com.morcinek.xpense.create.Validator
 import com.morcinek.xpense.data.project.Project
 import com.morcinek.xpense.data.project.ProjectManager
 import kotlinx.android.synthetic.main.project.*
+import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.__TextWatcher
 import org.jetbrains.anko.textChangedListener
 import javax.inject.Inject
@@ -23,10 +29,10 @@ class ProjectActivity : CreateActivity<Project>() {
     lateinit var projectManager: ProjectManager
 
     override var item: Project
-        get() = Project(nameEditText.getTrimString(), locationEditText.getTrimString(), currencyEditText.getTrimString())
+        get() = Project(nameEditText.getTrimString(), "", currencyEditText.getTrimString())
         set(value) {
             nameEditText.setText(value.name)
-            locationEditText.setText(value.location)
+            //            locationEditText.setText(value.location)
             currencyEditText.setText(value.currency)
         }
 
@@ -52,8 +58,26 @@ class ProjectActivity : CreateActivity<Project>() {
             }
         }
         nameEditText.textChangedListener(init)
-        locationEditText.textChangedListener(init)
         currencyEditText.textChangedListener(init)
+        locationButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                try {
+                    val intent = PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY).setFilter(autocompleteFilter()).build(this@ProjectActivity)
+                    startActivityForResult(intent, 0)
+                } catch (e: Exception) {
+                    Snackbar.make(view, R.string.google_play_services_error, Snackbar.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
+
+    private fun autocompleteFilter() = AutocompleteFilter.Builder().setTypeFilter(AutocompleteFilter.TYPE_FILTER_CITIES).build()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            val place = PlaceAutocomplete.getPlace(this, data);
+        }
     }
 
     override fun restoreItem(bundle: Bundle) = bundle.getParcelable<Project>()
