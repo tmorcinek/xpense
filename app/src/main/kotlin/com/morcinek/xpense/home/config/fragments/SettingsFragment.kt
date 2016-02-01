c9d94ae19930e6c0849027bcf6c218e9fc4d2cc1
Tomasz Morcinek <tomasz@morcinek.co.uk>
2016-02-01 14:16:43 +0700

    Fixing settings Fragment and turning of visibility.

--- app/src/main/kotlin/com/morcinek/xpense/home/config/fragments/SettingsFragment.kt
+++ app/src/main/kotlin/com/morcinek/xpense/home/config/fragments/SettingsFragment.kt
@@ -2,8 +2,8 @@ package com.morcinek.xpense.home.config.fragments
 
 import android.os.Bundle
 import android.preference.PreferenceFragment
-import android.support.v7.app.AppCompatActivity
 import com.morcinek.xpense.R
+import com.morcinek.xpense.common.utils.betterpickers.setTitle
 
 /**
  * Copyright 2016 Tomasz Morcinek. All rights reserved.
@@ -13,7 +13,6 @@ class SettingsFragment : PreferenceFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
-
-        (activity as AppCompatActivity).supportActionBar.setTitle(R.string.settings_label)
+        setTitle(R.string.settings_label)
     }
 }
